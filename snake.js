157a46362f8ebc35f4813fec3dc31438de35a3b9
Jérome EAP <jerome.eap@epitech.eu>
2022-05-01 15:26:09 +0900

    [ADDED] snake movement

--- snake.js
+++ snake.js
@@ -121,3 +121,52 @@ onKeyPress("right", () => {
     current_direction = directions.RIGHT;
   }
 });
+
+let move_delay = 0.2;
+let timer = 0;
+onUpdate(() => {
+  if (!run_action) return;
+  timer += dt();
+  if (timer < move_delay) return;
+  timer = 0;
+
+  let move_x = 0;
+  let move_y = 0;
+
+  switch (current_direction) {
+    case directions.DOWN:
+      move_x = 0;
+      move_y = block_size;
+      break;
+    case directions.UP:
+      move_x = 0;
+      move_y = -1 * block_size;
+      break;
+    case directions.LEFT:
+      move_x = -1 * block_size;
+      move_y = 0;
+      break;
+    case directions.RIGHT:
+      move_x = block_size;
+      move_y = 0;
+      break;
+  }
+
+  // Get the last element (the snake head)
+  let snake_head = snake_body[snake_body.length - 1];
+
+  snake_body.push(
+    add([
+      rect(block_size, block_size),
+      pos(snake_head.pos.x + move_x, snake_head.pos.y + move_y),
+      color(0, 0, 255),
+      area(),
+      "snake",
+    ])
+  );
+
+  if (snake_body.length > snake_length) {
+    let tail = snake_body.shift(); // Remove the last of the tail
+    destroy(tail);
+  }
+});
